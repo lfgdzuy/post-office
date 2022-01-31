0833d9725b14fd6abb8f8f6bd13fca8f82db4fc0
Leo <lfgdzdev@gmail.com>
2022-01-30 22:26:45 -0300

    Add baseline
    Includes:
    
    - Vue 3
    - Typescript
    - Vuex
    - Vue Router
    - Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    module.exports = {
+      content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+      ],
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+    }
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
